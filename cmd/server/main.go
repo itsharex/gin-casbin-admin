e5309ce4bc5a85af6c0860bb9c5810c1837beefb
wxl <297750182@qq.com>
2025-02-19 16:39:32 +0800

    feat(server): 重构 main 函数并添加数据库迁移功能
    -重构 main 函数，使用 urfave/cli 库创建命令行应用
    - 添加数据库迁移功能，支持 up 和 down 操作
    - 实现优雅关闭服务器的逻辑
    - 使用 embed 包嵌入迁移文件

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"flag"
+	"context"
+	"embed"
+	"errors"
 	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
+	"github.com/urfave/cli/v2"
 	"github.com/wxlbd/gin-casbin-admin/cmd/server/wire"
 	_ "github.com/wxlbd/gin-casbin-admin/docs" // 导入 swagger docs
 	"github.com/wxlbd/gin-casbin-admin/pkg/config"
@@ -29,22 +41,153 @@ import (
 // @securityDefinitions.apikey Bearer
 // @in header
 // @name Authorization
-func main() {
-	envConf := flag.String("conf", "configs/config.yaml", "config path, eg: -conf ./configs/config.yaml")
-	flag.Parse()
-	conf, err := config.NewConfig(*envConf)
-	if err != nil {
-		panic(err)
-	}
-	logger := log.NewLog(&conf.Log)
 
+//go:embed migrations/*.sql
+var migrationFS embed.FS
+
+var (
+	configFile string
+	conf       *config.Config
+	logger     *log.Logger
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "gin-casbin-admin",
+		Usage: "一个基于 Gin + Casbin 的权限管理系统",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "config",
+				Aliases:     []string{"c"},
+				Value:       "configs/config.yaml",
+				Usage:       "配置文件路径",
+				Destination: &configFile,
+			},
+		},
+		Before: func(c *cli.Context) error {
+			// 初始化配置和日志
+			var err error
+			conf, err = config.NewConfig(configFile)
+			if err != nil {
+				return err
+			}
+			logger = log.NewLog(&conf.Log)
+			return nil
+		},
+		Commands: []*cli.Command{
+			{
+				Name:    "start",
+				Aliases: []string{"s"},
+				Usage:   "启动服务",
+				Action:  startServer,
+			},
+			{
+				Name:  "migrate",
+				Usage: "数据库迁移",
+				Subcommands: []*cli.Command{
+					{
+						Name:   "up",
+						Usage:  "执行向上迁移",
+						Action: migrateUp,
+					},
+					{
+						Name:   "down",
+						Usage:  "执行向下迁移（回滚）",
+						Action: migrateDown,
+					},
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		logger.Error("app run error", zap.Error(err))
+	}
+}
+
+func startServer(c *cli.Context) error {
 	app, cleanup, err := wire.NewWire(conf, logger)
-	defer cleanup()
 	if err != nil {
-		panic(err)
+		return err
 	}
-	logger.Info("server start", zap.String("host", fmt.Sprintf("http://%s:%d", conf.Server.Host, conf.Server.Port)))
-	if err = app.Run(fmt.Sprintf("%s:%d", conf.Server.Host, conf.Server.Port)); err != nil {
-		panic(err)
+	defer cleanup()
+
+	// 创建 http.Server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf("%s:%d", conf.Server.Host, conf.Server.Port),
+		Handler: app,
 	}
+
+	// 创建一个通道来接收系统信号
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		logger.Info("server start", zap.String("host", fmt.Sprintf("http://%s:%d", conf.Server.Host, conf.Server.Port)))
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Error("server run error", zap.Error(err))
+			quit <- syscall.SIGTERM
+		}
+	}()
+
+	// 等待退出信号
+	<-quit
+	logger.Info("shutting down server...")
+
+	// 创建一个5秒超时的上下文
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// 优雅关闭服务器
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.Error("server forced to shutdown", zap.Error(err))
+		return err
+	}
+
+	logger.Info("server exited")
+	return nil
+}
+
+func getMigrate() (*migrate.Migrate, error) {
+	// 使用嵌入的迁移文件创建 source.Driver
+	d, err := iofs.New(migrationFS, "migrations")
+	if err != nil {
+		return nil, fmt.Errorf("failed to create iofs driver: %w", err)
+	}
+
+	// 构建数据库 DSN
+	dsn := fmt.Sprintf("mysql://%s",
+		conf.Database.DSN)
+
+	// 创建 migrate 实例
+	m, err := migrate.NewWithSourceInstance("iofs", d, dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create migrate instance: %w", err)
+	}
+
+	return m, nil
+}
+
+func migrateUp(c *cli.Context) error {
+	m, err := getMigrate()
+	if err != nil {
+		return err
+	}
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return fmt.Errorf("failed to migrate up: %w", err)
+	}
+	logger.Info("database migration up succeeded")
+	return nil
+}
+
+func migrateDown(c *cli.Context) error {
+	m, err := getMigrate()
+	if err != nil {
+		return err
+	}
+	if err := m.Down(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return fmt.Errorf("failed to migrate down: %w", err)
+	}
+	logger.Info("database migration down succeeded")
+	return nil
 }
